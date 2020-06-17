b36b42ebd75d80355068fd97c24f0bb5365c6c7f
Willian <williang181@gmail.com>
2020-06-17 17:03:54 -0300

    mobile api

--- be-the-hero/mobile/src/pages/Detail/index.js
+++ be-the-hero/mobile/src/pages/Detail/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Text, Image, TouchableOpacity }  from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking }  from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
 import styles from './styles';
 import logoImg from '../../assets/logo.png';
 
 export default function Detail(){
     const navigation = useNavigation();
+    const message = 'Olá, isso é o corpo do email.';
 
     function navigateBack(){
         navigation.goBack()
+    }
+
+    function sendMail(){
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Cadelinha atropelada',
+            recipients: ['williang181@gmail.com'],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp(){
+        Linking.openURL(`whatsapp://send?=554196041538&text=${message}`);
     }
 
     return(
@@ -39,11 +53,11 @@ export default function Detail(){
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>WhatsApp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
--- be-the-hero/mobile/src/pages/Incidents/index.js
+++ be-the-hero/mobile/src/pages/Incidents/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, FlatList, Image, Text, TouchableOpacity }  from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
+import api from '../../services/api';
 
 export default function Incidents(){
+    const [incidents, setIncidents] = useState([]);
     const navigation = useNavigation();
 
     function navigateToDetail(){
         navigation.navigate('Detail');
+    }
+
+    async function loadIncidents(){
+        const response = await api.get('incidents');
+
+        setIncidents(response.data);
     }
 
     return(
@@ -25,20 +33,20 @@ export default function Incidents(){
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
             <FlatList
-                data={[1, 2, 3]}
+                data={[incidents]}
                 style={styles.incidentsList}
-                keyExtractor={incident => String(incident)}
+                keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                renderItem={({item: incident}) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>LBV</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
-                        <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>LBV</Text>
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
-                        <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>LBV</Text>
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                <Text style={styles.incidentValue}>{incident.value}</Text>
 
                         <TouchableOpacity
                             style={styles.detailsButton}
