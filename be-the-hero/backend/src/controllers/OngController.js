fd18092ff74faa0d1eea2012ed4d3c10b7e2e0a2
Willian <williang181@gmail.com>
2020-06-23 15:42:45 -0300

    tests

--- be-the-hero/backend/src/controllers/OngController.js
+++ be-the-hero/backend/src/controllers/OngController.js
@@ -1,3 +1,4 @@
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 const crypto = require('crypto');
 
@@ -12,17 +13,17 @@ module.exports = {
     async create(request, response){
         const {name, email, whatsapp, city, uf} = request.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    });
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        });
 
-    return response.json({id});
+        return response.json({id});
     }
 };
